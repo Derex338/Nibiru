7821b24a29dffc1faea305f7b3e253d41c0b2f23
Mr. Samuel <82917953+lermal@users.noreply.github.com>
2026-08-08 00:38:30 +0300

    Unblock Release CI warnings-as-errors
    Allow known RA/CS noise under TreatWarningsAsErrors and fix sealed SharedWorkbenchSystem protected deps.

--- Content.Shared/_Nibiru/Workbench/SharedWorkbenchSystem.cs
+++ Content.Shared/_Nibiru/Workbench/SharedWorkbenchSystem.cs
@@ -1,24 +1,17 @@
-using System.Linq;
 using Content.Shared.Construction.Components;
-using Robust.Shared.Map;
 using Robust.Shared.Prototypes;
-using static Content.Shared.Interaction.SharedInteractionSystem;
 
 namespace Content.Shared._Nibiru.Workbench;
 
-public sealed class SharedWorkbenchSystem : EntitySystem
+public sealed partial class SharedWorkbenchSystem : EntitySystem
 {
-    [Dependency] private readonly IMapManager _mapManager = default!;
-    [Dependency] private readonly SharedMapSystem _map = default!;
-    [Dependency] protected readonly IPrototypeManager PrototypeManager = default!;
-    [Dependency] protected readonly SharedTransformSystem TransformSystem = default!;
+    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
 
     public string GetExamineName(GenericPartInfo info)
     {
         if (info.ExamineName is not null)
             return Loc.GetString(info.ExamineName.Value);
 
-        return PrototypeManager.Index(info.DefaultPrototype).Name;
+        return _prototypeManager.Index(info.DefaultPrototype).Name;
     }
 }
-
